789737e14875dd33f92ef29018e23a303c9caf57
gelho-hub <siklosiagellert@gmail.com>
2024-03-12 20:24:23 +0100

    created test project

---
+++ StringManipulationApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace StringManipulationApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Modulator modulator = new Modulator();
+            modulator.FirstStringModulator("madárnyelv");
+            Console.WriteLine("Nyomj meg egy gombot a folytatáshoz!");
+            Console.ReadKey();
+            modulator.Squeeze("megadott sztring", "gt");
+        }
+    }
+}
